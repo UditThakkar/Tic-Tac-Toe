4f4bf7c1e0e3b95c05cf84c202eb0371997776e7
Udit Thacker <65917694+UditThakkar@users.noreply.github.com>
2021-10-13 19:33:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+console.log("Welcome to the game")
+let audioturn = new Audio("ting.mp3")
+let gameover = new Audio("gameover.mp3")
+let turn = "X"
+let isgameover = false
+
+const changeturn = ()=>{
+    return turn === "X"? "0": "X"
+}
+
+// check for a winning
+const checkWin = ()=>{
+    let boxtext = document.getElementsByClassName('boxtext');
+    let wins = [
+       [0,1,2],
+       [3,4,5],
+       [6,7,8],
+       [0,3,6],
+       [1,4,7],
+       [2,5,8],
+       [0,4,8],
+       [2,4,6], 
+    ]
+    wins.forEach(e=>{
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[1]].innerText === boxtext[e[2]].innerText) &&  (boxtext[e[0]].innerText !== "")){
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
+            isgameover = true;
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px"
+        }
+    })
+}
+
+//  game logic
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element=>{
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', (e)=>{
+        if(boxtext.innerText === ''){
+            boxtext.innerText = turn;
+            turn = changeturn();
+            audioturn.play();
+            checkWin();
+            if(!isgameover){
+                document.getElementsByClassName("info")[0].innerText = "Turn for "+ turn;
+            }
+            
+        }
+    })
+})
+
+//  setting up reset button
+reset.addEventListener('click',()=>{
+    let boxtexts = document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element=>{
+        element.innerText = ""
+    })
+    turn = "X"
+    isgameover = false;
+    document.getElementsByClassName("info")[0].innerText = "Turn for "+ turn;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
+})
